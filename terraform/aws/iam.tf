b0ebacb6a59b2d368ecddf428feca56bcc70e858
shunsei <k.shunsei1227@gmail.com>
2025-09-19 11:49:20 +0900

    add fluent bit iam

--- terraform/aws/iam.tf
+++ terraform/aws/iam.tf
@@ -61,3 +61,40 @@ resource "aws_eks_pod_identity_association" "loki" {
     aws_iam_role_policy_attachment.loki_s3_access_attach,
   ]
 }
+
+resource "aws_iam_role" "fluent_bit_pod_identity_role" {
+  name = "FluentBitPodIdentityRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "pods.eks.amazonaws.com"
+        },
+        Action = [
+          "sts:TagSession",
+          "sts:AssumeRole",
+        ],
+      },
+    ],
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "fluent_bit_cloudwatch_access_attach" {
+  role       = aws_iam_role.fluent_bit_pod_identity_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+resource "aws_eks_pod_identity_association" "fluent_bit" {
+  cluster_name    = var.cluster_name
+  namespace       = "amazon-cloudwatch"
+  service_account = "fluent-bit"
+  role_arn        = aws_iam_role.fluent_bit_pod_identity_role.arn
+
+  depends_on = [
+    # IAM ロールを先に作成するため
+    aws_iam_role_policy_attachment.fluent_bit_cloudwatch_access_attach,
+  ]
+}
